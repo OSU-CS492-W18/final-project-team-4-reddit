26f6270f39bce11517a84e574f525b79f87eda12
DESKTOP-JJ9IVB6\Connor Sedwick <sedwickc@oregonstate.edu>
2018-03-21 14:15:06 -0700

    Adds Settings

---
+++ app/src/main/java/com/example/learntoprogram/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.example.learntoprogram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Connor Sedwick on 3/21/2018.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+}
---
+++ app/src/main/java/com/example/learntoprogram/SettingsFragment.java
@@ -0,0 +1,48 @@
+package com.example.learntoprogram;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.EditTextPreference;
+
+/**
+ * Created by Connor Sedwick on 3/21/2018.
+ */
+
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.filter);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_text_color_key))) {
+            EditTextPreference textColorPref = (EditTextPreference)findPreference(key);
+            textColorPref.setSummary(textColorPref.getText());
+        }
+        if (key.equals(getString(R.string.pref_theme_key))) {
+            EditTextPreference themePref = (EditTextPreference)findPreference(key);
+            themePref.setSummary(themePref.getText());
+        }
+    }
+
+    @Override
+    public void onResume() {
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
+}
